18ccc79b7c1f03bf52ea5bccc4f7b7c8fd5853f9
Shota Okugawa <okusho721@gmail.com>
2021-12-22 16:02:49 +0900

    try subprocess

---
+++ test.py
@@ -0,0 +1,8 @@
+import subprocess
+
+val1 = 1
+val2 = 2
+
+print("hello")
+subprocess.call(
+    ['/Users/okugawashota/Desktop/testShellscript/test.sh'])
--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 
-manual[0]="MarioCart/MarioCart_matchmake.py"
-switch[0]="00:00:00:00"
+manual[0]="手順書0"
+manual[1]="手順書1"
+manual[2]="手順書2"
+manual[3]="手順書3"
+manual[4]="手順書4"
+
+switch[0]="switch0"
+switch[1]="switch1"
+switch[2]="switch2"
+switch[3]="switch3"
+switch[4]="switch4"
 
 
 
@@ -14,9 +23,10 @@ switch[0]="00:00:00:00"
 function switch_auto_check(){
   echo ${manual[$1]}
   echo ${switch[$2]}
-  echo $'\a\a\a'
 }
 
 # switch_auto_check($1, $2)
 
-switch_auto_check $1 $2
+# switch_auto_check $1 $2
+
+echo "This is a test."
